b950b16a79c6cbece6105f70466d996d4d92f04e
the-cross-art <imrannaz326@gmail.com>
2020-11-04 17:41:36 +0530

    mongoDB Connected

---
+++ quiz/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+
+from .models import Question, Choice
+from .forms import QuestionForm, ChoiceForm, ChoiceInlineFormset
+# Register your models here.
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    can_delete = False
+    max_num = Choice.MAX_CHOICES_COUNT
+    min_num = Choice.MAX_CHOICES_COUNT
+    form = ChoiceForm
+    formset = ChoiceInlineFormset
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    model = Question
+    inlines = (ChoiceInline, )
+    list_display = ['html', 'is_published']
+    list_filter = ['is_published']
+    search_fields = ['html', 'choices__html']
+    actions = None
+    form = QuestionForm
+
+
+admin.site.register(Question, QuestionAdmin)
